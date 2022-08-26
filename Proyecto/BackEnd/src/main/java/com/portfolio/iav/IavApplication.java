a77c41a1d8270a18c2568573af674fcecb96e737
Ignacio Valdivia <igna.a.valdivia@gmail.com>
2022-08-26 16:41:13 -0300

    carpeta backend

---
+++ Proyecto/BackEnd/src/main/java/com/portfolio/iav/IavApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.iav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IavApplication.class, args);
+	}
+
+}
---
+++ Proyecto/BackEnd/src/test/java/com/portfolio/iav/IavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.iav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
