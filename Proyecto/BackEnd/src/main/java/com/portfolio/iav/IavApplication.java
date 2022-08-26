bf6ed8bafe73561a1ab0857187be4dc486f27035
Ignacio Valdivia <igna.a.valdivia@gmail.com>
2022-08-26 18:39:25 -0300

    Spring

--- Proyecto/BackEnd/src/main/java/com/portfolio/iav/IavApplication.java
+++ Proyecto/BackEnd/src/main/java/com/portfolio/iav/IavApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IavApplication {
 
-	public static void main(String[] args) {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
 		SpringApplication.run(IavApplication.class, args);
 	}
-
 }
