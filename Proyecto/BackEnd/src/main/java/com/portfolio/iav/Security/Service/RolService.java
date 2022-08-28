acad61c69229937a070c0d2e360220acbb80a66c
Ignacio Valdivia <igna.a.valdivia@gmail.com>
2022-08-27 22:44:24 -0300

    avances

---
+++ Proyecto/BackEnd/src/main/java/com/portfolio/iav/Security/Service/RolService.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.iav.Security.Service;
+
+import com.portfolio.iav.Security.Entity.Rol;
+import com.portfolio.iav.Security.Enums.RolNombre;
+import com.portfolio.iav.Security.Repository.iRolRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Igna
+ */
+@Service
+@Transactional
+public class RolService {
+    @Autowired
+    iRolRepository irolRepository;
+    
+    public Optional<Rol> getByRolNombre(RolNombre rolNombre){
+        return irolRepository.findByRolNombre(rolNombre);
+    }
+    
+    public void save(Rol rol){
+        irolRepository.save(rol);
+    }
+    
+}
